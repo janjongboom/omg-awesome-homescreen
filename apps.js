35a99c285641aaefb0a6800c540390a23cbbe107
Jan Jongboom <janjongboom@gmail.com>
2014-05-28 09:31:27 +0200

    initial

---
+++ apps.js
@@ -0,0 +1,17 @@
+navigator.mozApps.mgmt.getAll().onsuccess = function(ev) {
+  ev.target.result.forEach(function(app) {
+    var li = document.createElement('li')
+    li.textContent = app.manifest.name;
+
+    li.onclick = function() {
+      app.launch();
+    }
+
+    var img = document.createElement('img');
+    img.src = app.origin + app.manifest.icons['120'];
+
+    li.appendChild(img);
+
+    document.querySelector('#apps').appendChild(li);
+  })
+}
